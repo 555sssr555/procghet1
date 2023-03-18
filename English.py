791b2866350fb6b1352e60b9185336025af23568
555sssr555 <71327364+555sssr555@users.noreply.github.com>
2023-03-19 00:42:31 +0200

    Add files via upload

---
+++ English.py
@@ -0,0 +1,20 @@
+"""Словарь DIC"""
+dic = {
+    "end": "конец",
+    "for": "для",
+    "as": "в качестве",
+    "assert": "утверждать",
+    "break": "перерыв",
+    "class": "класс",
+    "or": "или",
+}
+
+while len(dic):# Подсчет словаря
+    for key in dic.keys() and dic.values():# Ключ : значения
+        print(key, "\n")
+        x = input("Перевод: ")
+        if x != dic.keys:
+            pass
+    exit()
+
+
